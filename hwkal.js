3787f49ebf77c691f48b1ddb36fd2570c15fb92f
HW MODS WA HAIKAL <89538075+HW-XMTeam@users.noreply.github.com>
2022-10-06 07:17:37 +0700

    Add files via upload

--- hwkal.js
+++ hwkal.js
@@ -22,16 +22,16 @@ global.linkpro = 'mailto:support@whatsapp.com?subject=Deactivate%20My%20Account%
 global.email = 'denyp857@gmail.com'
 global.region = 'indonesia'
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'HW MODS WA' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'HW MODS WA' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'HW MODS WA' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'ℕaᷡʍͦę' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'ℕaᷡʍͦę' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'ℕaᷡʍͦę' //ubah jadi nama mu, note tanda ' gausah di hapus!
 //=================================================//
 // Other
 global.owner = ['6281214281312']
 global.premium = ['6281214281312']
-global.packname = 'HW MODS WA'
-global.ownerr = ['HW MODS WA']
-global.author = 'HW MODS WA'
+global.packname = 'ℕaᷡʍͦę'
+global.ownerr = ['ℕaᷡʍͦę']
+global.author = 'ℕaᷡʍͦę'
 global.sessionName = './sessionye/haikal'
 global.prefa = ['','!','.',',','🐤','🗿']
 global.sp = '⭔'
